250576f130ba3fbd8264e8a4b108005d7eede36c
muzaffarmaniyazov-ui <muzaffarmaniyazov@icloud.com>
2026-03-25 16:59:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -742,5 +742,5 @@ def ice(data):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     socketio.run(app, host="0.0.0.0", port=port, debug=False)
